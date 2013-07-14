504719eef0528c6d9e1cc2cba20359f661c9693c
Josh Cheek <josh.cheek@gmail.com>
2013-07-14 16:12:17 -0500

    Read in cars

--- car-inventory.hs
+++ car-inventory.hs
@@ -1,4 +1,16 @@
-import System.IO (hFlush, stdout)
+import System.IO (hFlush, stdout, readFile)
+
+data Car = Car { year           :: Integer
+               , make           :: String
+               , model          :: String
+               , style          :: String
+               , color          :: String
+               , mileage        :: String
+               , airConditioner :: Bool
+               , vinNumber      :: String
+               , price          :: Double
+               , previousOwner  :: String
+               } deriving Show
 
 optionAddCar             = 1 :: Integer
 optionRemoveCar          = 2 :: Integer
@@ -8,6 +20,8 @@ optionDisplayCarsByPrice = 5 :: Integer
 optionDisplayCarsByColor = 6 :: Integer
 optionDisplayAllCars     = 7 :: Integer
 optionQuit               = 8 :: Integer
+options                  = [optionAddCar, optionRemoveCar, optionDisplayCar, optionDisplayCarsByYear, optionDisplayCarsByPrice, optionDisplayCarsByColor, optionDisplayAllCars, optionQuit]
+optionPrompt             = "Enter your choice (1-8) ===> "
 
 printMenu :: IO ()
 printMenu = do
@@ -37,45 +51,76 @@ printMenu = do
   putStrLn $ (show optionQuit)               ++ "  -  Quit. (exit inventory tracking system)"
   putStrLn "______________________________________________"
 
+-- returns an option
+-- reprompts user if input is invalid (unless it's non-numeric, not sure how to deal with that)
 getOption :: IO Integer
 getOption = do
-  putStr "Enter your choice (1-8) ===> "
+  putStr optionPrompt
   hFlush stdout
   option <- getLine
-  return $ read option
+  if elem (read option) options
+    then return (read option)
+    else getOption
 
-handleOption :: Integer -> IO Bool
-handleOption option
+handleOption :: [Car] -> Integer -> IO ([Car], Bool)
+handleOption cars option
   | option == optionAddCar = do
       putStrLn "ADD A NEW CAR"
-      return False
+      return (cars, False)
   | option == optionRemoveCar = do
       putStrLn "REMOVE CAR"
-      return False
+      return (cars, False)
   | option == optionDisplayCar = do
       putStrLn "DISPLAY CAR"
-      return False
+      return (cars, False)
   | option == optionDisplayCarsByYear = do
       putStrLn "DISPLAY CAS BY YEAR"
-      return False
+      return (cars, False)
   | option == optionDisplayCarsByPrice = do
       putStrLn "DISPLAY CARS BY PRICE"
-      return False
+      return (cars, False)
   | option == optionDisplayCarsByColor = do
       putStrLn "DISPLAY CARS BY COLOUR"
-      return False
+      return (cars, False)
   | option == optionDisplayAllCars = do
       putStrLn "DISPLAY ALL CARS"
-      return False
+      return (cars, False)
   | option == optionQuit = do
       putStrLn "QUITTING"
-      return True
+      return (cars, True)
+
+readCars :: String -> IO [Car]
+readCars filename = do
+  carFileData <- readFile filename
+  return $ map carFromData (carData $ lines carFileData)
+  where
+    carData carFileData
+      | length carFileData < 9 = []
+      | otherwise              = [(take 8  $ fst $ splitAt 9 carFileData)] ++
+                                  (carData $ snd $ splitAt 9 carFileData)
+    carFromData [line1, style, color, miles, ac, vin, price, prevOwner] =
+      Car { year            = read ((words line1) !! 0)
+          , make            =       (words line1) !! 1
+          , model           =       (words line1) !! 2
+          , style           = style
+          , color           = color
+          , mileage         = miles
+          , airConditioner  = if ac == "Y" then True else False
+          , vinNumber       = vin
+          , price           = read price
+          , previousOwner   = prevOwner
+          }
+
+menuLoop :: [Car] -> IO ()
+menuLoop cars = do
+  printMenu
+  option <- getOption
+  (newCars, shouldQuit) <- handleOption cars option
+  if shouldQuit
+    then return ()
+    else menuLoop newCars
 
 main :: IO ()
 main = do
-  printMenu
-  option     <- getOption
-  shouldQuit <- handleOption option
-  if not shouldQuit
-    then main
-    else return ()
+  cars <- readCars "inventory.data"
+  menuLoop cars
